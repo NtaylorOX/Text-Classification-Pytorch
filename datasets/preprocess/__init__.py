f664273ae71758e4327c1f9568012d1aa2e80d72
NTaylorOX <niall.taylor@psych.ox.ac.uk>
2022-08-17 09:52:44 +0100

    refactor

--- datasets/preprocess/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import get_clean_text
-from .document import run_prepro as run_doc_prepro
-from .sentence import run_prepro as run_sent_prepro
--- models/TextCNN/__init__.py
+++
@@ -1,2 +0,0 @@
-from .cnn1d import TextCNN1D
-from .cnn2d import TextCNN2D
